1dedf505ccb452fe3dd51aa53c8443d57ca48777
Vittutexx <vitortex06@gmail.com>
2024-03-04 16:37:45 -0300

    Modulo 02. Atv 03.

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,16 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Olá Mundo!");
+        System.out.println("Esse é o Screen Match");
+        System.out.println("Filme: Top Gun: Maverick");
+
+        int anoDeLancamento = 2022;
+        System.out.println("Ano de Lançamento: " + anoDeLancamento);
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 8.1;
+        System.out.println("Nota do Filme: " + notaDoFilme);
+
+
 
     }
 }
