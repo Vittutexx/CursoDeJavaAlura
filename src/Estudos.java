b905c3a55e7b739fc06968c84fec03cd32175559
Vittutexx <vitortex06@gmail.com>
2024-03-04 16:20:37 -0300

    Criação dos exercicios feitos pela Alura.

--- src/Estudos.java
+++ src/Estudos.java
@@ -1,2 +1,9 @@
 public class Estudos {
-}
+    public static void main(String[] Args) {
+
+    System.out.println("Coisas para estudar: ");
+    System.out.println("Java");
+    System.out.println("Python");
+
+    }//main
+}//class
---
+++ src/Soma.java
@@ -0,0 +1,12 @@
+
+
+
+public class Soma {
+
+    public static void main(String[] Args) {
+
+        System.out.println(10 + 5);
+    }
+}
+
+
